5c6b2bdfec56473772e1b385c00026b974425abd
RodrigoLimaRFL <guico21@gmail.com>
2023-06-03 23:35:09 -0300

    contact and create account pages

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -8,15 +8,61 @@ import Header from "../../Components/Header/Header";
 import Label from "../../Components/Label/Label";
 
 const Contact = () => {
+    // contact variables
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+
+    // confirmation message
+    const [sentMessage, setSentMessage] = React.useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            name: name,
+            email: email,
+            message: message
+        }
+
+        // converts the data to JSON
+        const jsonData = JSON.stringify(data);
+
+        console.log(jsonData);
+
+        // note: there is no fetch because there is no backend to receive POST requests
+
+        // resets the form
+        setName("");
+        setEmail("");
+        setMessage("");
+        setSentMessage("Mensagem enviada com sucesso!");
+    }
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    }
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const handleMessageChange = (event) => {
+        setMessage(event.target.value);
+    }
+
     return (
         <>
             <Header />
             <h2>Entre em contato conosco</h2>
             <p>Preencha o formulário abaixo para entrar em contato com a nossa loja.</p>
-            <Label id="nome" type="text"> Nome: </Label> <br></br>
-            <Label id="email" type="email"> Email: </Label> <br></br>
-            <Label id="mensagem" type="text"> Mensagem: </Label> <br></br>
-            <Button> Enviar mensagem </Button>
+            <form onSubmit={handleSubmit}>
+                <Label id="nome" type="text" onChange={handleNameChange}> Nome: </Label> <br></br>
+                <Label id="email" type="email" onChange={handleEmailChange}> Email: </Label> <br></br>
+                <Label id="mensagem" type="text" onChange={handleMessageChange}> Mensagem: </Label> <br></br>
+                <Button> Enviar mensagem </Button>
+                {sentMessage}
+            </form>
             <Footer />
         </>
     );
--- src/Pages/CreateAccount/CreateAccount.jsx
+++ src/Pages/CreateAccount/CreateAccount.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import Footer from "../../Components/Footer/Footer";
 import Header from "../../Components/Header/Header";
@@ -8,17 +9,94 @@ import "./CreateAccount.css";
 import Button from "../../Components/Button/Button";
 
 const CreateAccount = () => {
+    // account variables
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [confirmPassword, setConfirmPassword] = React.useState("");
+    const [phone, setPhone] = React.useState("");
+    const [address, setAddress] = React.useState("");
+
+    const navigate = useNavigate();
+
+    // password != confirmPassword error
+    const [passwordError, setPasswordError] = React.useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if(password !== confirmPassword)
+        {
+            setPasswordError("As senhas não coincidem.");
+            return;
+        }
+
+        const data = {
+            name: name,
+            email: email,
+            password: password,
+            phone: phone,
+            address: address
+        }
+
+        // converts the data to JSON
+        const jsonData = JSON.stringify(data);
+
+        console.log(jsonData);
+
+        // note: there is no fetch because there is no backend to receive POST requests
+
+        // resets the form
+        setName("");
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
+        setPhone("");
+        setAddress("");
+
+        setPasswordError("");
+
+        navigate("/login");
+    }
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    }
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const handleConfirmPasswordChange = (event) => {
+        setConfirmPassword(event.target.value);
+    }
+
+    const handlePhoneChange = (event) => {
+        setPhone(event.target.value);
+    }
+
+    const handleAddressChange = (event) => {
+        setAddress(event.target.value);
+    }
+
     return (
         <>
             <Header />
             <h1>Criar Conta</h1>
-            <Label id="nome" type="text" required> Nome completo: </Label> <br></br>
-            <Label id="email" type="email" required> Email: </Label> <br></br>
-            <Label id="senha" type="password" required> Senha: </Label> <br></br>
-            <Label id="confirmarSenha" type="password" required> Confirmar Senha: </Label> <br></br>
-            <Label id="telefone" type="tel" required> Telefone: </Label> <br></br>
-            <Label id="endereco" type="text" required> Endereço: </Label> <br></br>
-            <Button> Criar Conta </Button>
+            <form onSubmit={handleSubmit}>
+                <Label id="nome" type="text" required onChange={handleNameChange}> Nome completo: </Label> <br></br>
+                <Label id="email" type="email" required onChange={handleEmailChange}> Email: </Label> <br></br>
+                <Label id="senha" type="password" required onChange={handlePasswordChange}> Senha: </Label> <br></br>
+                <Label id="confirmarSenha" type="password" required onChange={handleConfirmPasswordChange}> Confirmar Senha: </Label> <br></br>
+                <Label id="telefone" type="tel" required onChange={handlePhoneChange}> Telefone: </Label> <br></br>
+                <Label id="endereco" type="text" required onChange={handleAddressChange}> Endereço: </Label> <br></br>
+                <Button> Criar Conta </Button>
+                {passwordError}
+            </form>
             <a href="/login"> Já possui uma conta? </a>
             <Footer />
         </>
